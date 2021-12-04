84e099738736b1f8c4117cce792cc3468f5ef0a7
Alec Brooks <perfunct@protonmail.com>
2021-12-04 20:18:25 +1100

    Day 4

---
+++ 4.hs
@@ -0,0 +1,37 @@
+import Data.List
+import Data.List.Split
+
+parseBoards  :: [String] -> [[[String]]]
+parseBoards lines = map (map words) $ splitOn [""] $ tail $ tail lines
+
+checkBoardIsWinner :: [String] -> [[String]] -> Bool
+checkBoardIsWinner [] _ = False
+checkBoardIsWinner draws board = any id (horizontalBingos ++ verticalBingos)
+    where horizontalBingos = map (all (`elem` draws)) board
+          verticalBingos = map (all (`elem` draws)) (transpose board)
+
+
+whenBoardWins :: [String] -> [[String]] -> Int
+whenBoardWins draws board = if checkBoardIsWinner draws board then
+                                whenBoardWins (init draws) board
+                            else 
+                                length draws
+
+scoreBoard :: [String] -> [[String]] -> Int -> Int
+scoreBoard draws board nDraws = sumOfUnmarked * finalNumber 
+    where sumOfUnmarked = (sum . map read . filter (`notElem`  take (nDraws+1) draws) . concat) board 
+          finalNumber = read $ draws !! nDraws
+
+main = do
+    content <- readFile "4.txt"
+    let linesOfFile = lines content
+    let draws = splitOn "," $ head linesOfFile
+    let boards = parseBoards linesOfFile
+    let boardsByScore = sortBy (\(_,a) (_,b) -> compare a b) $ zip boards $ map (whenBoardWins draws) boards
+    let winningBoard = head boardsByScore
+    let losingBoard = last boardsByScore
+
+    print $ scoreBoard draws (fst winningBoard) (snd winningBoard)
+    print $ scoreBoard draws (fst losingBoard) (snd losingBoard)
+
+
