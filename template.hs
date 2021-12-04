d9b1fe482d92fca5be56989c05c590df616eb42a
Alec Brooks <perfunct@protonmail.com>
2021-12-04 16:44:21 +1100

    Properly indent tempalte

--- template.hs
+++ template.hs
@@ -1,7 +1,7 @@
 main = do
-   content <- readFile "1.txt"
-   let linesOfFile = lines content
-   let ns = map read linesOfFile :: [Int]
+    content <- readFile "1.txt"
+    let linesOfFile = lines content
+    let ns = map read linesOfFile :: [Int]
 
-   print $ take 5 ns
+    print $ take 5 ns
 
